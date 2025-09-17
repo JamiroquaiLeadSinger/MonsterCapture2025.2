79947b887678a9d6eb49441b3366271805c91922
JamiroquaiLeadSinger <elltertainment@gmail.com>
2025-09-17 15:57:56 +1000

    more camera control

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -9,7 +9,12 @@ public class OrbitCamera : MonoBehaviour
     private Vector2 orbitAngles = new Vector2(45f, 0f);
     private Vector2 input;
 
-    public InputAction jetPackAction;
+    bool isCameraBlock = false;
+    Vector3 barrier;
+
+    public Transform focus;
+
+    // public InputAction jetPackAction;
     
     private void OnEnable()
     {
@@ -26,5 +31,54 @@ public class OrbitCamera : MonoBehaviour
     {
         input.x = -value.Get<Vector2>().y;
         input.y = value.Get<Vector2>().x;
+    }
+
+    bool ManualRotation()
+    {
+        float deadzone = 0.001f;
+        if(input.magnitude > deadzone)
+        {
+            orbitAngles += input * rotationSpeed * Time.unscaledDeltaTime;
+            return true;
+        }
+        return false;
+    }
+    
+    void OnZoom(InputValue value)
+    {
+        distance -= (value.Get<Vector2>().y) * 0.5f;
+
+        distance = Mathf.Clamp(distance, 0, 10);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(focus.position, -transform.forward, out hit, distance))
+        {
+            isCameraBlock = true;
+            barrier = hit.point + hit.normal * 0.2f;
+        }
+        else
+        {
+            isCameraBlock = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Quaternion lookRotation = transform.localRotation;
+
+        if (ManualRotation())
+        {
+            orbitAngles.x = Mathf.Clamp(orbitAngles.x, -70, 70);
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = isCameraBlock ? barrier : focus.position - lookDirection * distance;
+
+        transform.position = Vector3.Lerp(transform.position, lookPosition, Time.deltaTime * 50f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 50f);
     }
 }
